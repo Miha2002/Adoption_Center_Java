8039cc9ff46f967586fef0ace322a486b6e33ecc
Miha <iancu_miha_flori@yahoo.com>
2022-05-20 01:00:16 +0300

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import Animals.Cat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
         Cat cat1 = new Cat("Fluffy", "f", 10, 6, false, "tabby", "brown");
+
+
     }
 }
